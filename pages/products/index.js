21d403f569f77127f78ab2a40ff2cdd05e5cf883
SasmitaMaharana <smaharana50_be21@thapar.edu>
2023-12-31 19:10:18 +0530

    my new

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import {useState, useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 //import img1 from '@/public/product.png'
 
